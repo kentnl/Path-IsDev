f5e0824fc1d744c374dcd0a20d351ede6616a675
Kent Fredric <kentfredric@gmail.com>
2013-09-17 17:26:33 +1200

    Build results of b472abf (on master)

--- lib/Path/IsDev.pm
+++ lib/Path/IsDev.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Path::IsDev::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Path::IsDev::VERSION = '0.3.0';
+  $Path::IsDev::VERSION = '0.3.1';
 }
 
 # ABSTRACT: Determine if a given Path resembles a development source tree
@@ -57,7 +57,7 @@ Path::IsDev - Determine if a given Path resembles a development source tree
 
 =head1 VERSION
 
-version 0.3.0
+version 0.3.1
 
 =head1 SYNOPSIS
 
--- lib/Path/IsDev/Heuristic.pm
+++ lib/Path/IsDev/Heuristic.pm
@@ -6,7 +6,7 @@ BEGIN {
   $Path::IsDev::Heuristic::AUTHORITY = 'cpan:KENTNL';
 }
 {
-  $Path::IsDev::Heuristic::VERSION = '0.3.0';
+  $Path::IsDev::Heuristic::VERSION = '0.3.1';
 }
 
 # ABSTRACT: Heuristic Base class
@@ -79,7 +79,7 @@ Path::IsDev::Heuristic - Heuristic Base class
 
 =head1 VERSION
 
-version 0.3.0
+version 0.3.1
 
 =head1 METHODS
 
