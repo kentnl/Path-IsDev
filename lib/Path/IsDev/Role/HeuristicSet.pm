e07a4afe3936d4921decba3dae27dbd9a5555ced
Kent Fredric <kentfredric@gmail.com>
2013-10-06 06:16:32 +1300

    Add Heuristic Set Roles

---
+++ lib/Path/IsDev/Role/HeuristicSet.pm
@@ -0,0 +1,71 @@
+use strict;
+use warnings;
+
+package Path::IsDev::Role::HeuristicSet;
+
+# ABSTRACT: Role for sets of Heuristics.
+
+=begin MetaPOD::JSON v1.1.0
+
+{
+    "namespace":"Path::IsDev::Role::HeuristicSet",
+    "interface":"role"
+}
+
+=end MetaPOD::JSON
+
+=cut
+
+sub _use_module { require Module::Runtime; goto &Module::Runtime::use_module }
+sub _com_mn     { require Module::Runtime; goto &Module::Runtime::compose_module_name; }
+sub _debug      { require Path::IsDev;     goto &Path::IsDev::debug }
+
+use Role::Tiny;
+
+requires 'modules';
+
+sub _expand_heuristic {
+  my ( $self, $hn ) = @_;
+  return _com_mn( 'Path::IsDev::Heuristic', $hn );
+}
+
+sub _expand_negative_heuristic {
+  my ( $self, $hn ) = @_;
+  return _com_mn( 'Path::IsDev::NegativeHeuristic', $hn );
+}
+
+sub _load_module {
+  my ( $self, $module ) = @_;
+  return _use_module($module);
+}
+
+=method C<matches>
+
+Determine if the C<HeuristicSet> contains a match.
+
+    if( $hs->matches( $result_object ) ) {
+        # one of hs->modules() matched $result_object->path
+    }
+
+=cut
+
+sub matches {
+    my ( $self, $result_object ) = @_;
+TESTS: for my $module ( $self->modules ) {
+    $self->_load_module($module);
+    if ( $module->can('excludes') ) {
+      if ( $module->excludes($result_object) ) {
+        _debug( $module->name . q[ excludes path ] . $result_object->path );
+        return;
+      }
+      next TESTS;
+    }
+    next unless $module->matches($result_object);
+    my $name = $module->name;
+    _debug( $name . q[ matched path ] . $result_object->path );
+    return 1;
+  }
+  return;
+}
+
+1;
---
+++ lib/Path/IsDev/Role/HeuristicSet/Simple.pm
@@ -0,0 +1,67 @@
+
+use strict;
+use warnings;
+
+package Path::IsDev::Role::HeuristicSet::Simple;
+
+# ABSTRACT: Simple excludes/includes set
+
+=begin MetaPOD::JSON v1.1.0
+
+{
+    "namespace":"Path::IsDev::Role::HeuristicSet::Simple",
+    "interface":"role",
+    "does":"Path::IsDev::Role::HeuristicSet"
+}
+
+=end MetaPOD::JSON
+
+=requires C<heuristics>
+
+Consuming classes must provide this method,
+and return a list of shorthand Heuristics.
+
+    sub heuristics {
+        return qw( MYMETA )
+    }
+
+=cut
+
+=requires C<negative_heuristics>
+
+Consuming classes must provide this method,
+and return a list of shorthand Negative Heuristics.
+
+    sub negative_heuristics {
+        return qw( IsDev::IgnoreFile )
+    }
+
+=cut
+
+use Role::Tiny;
+
+with 'Path::IsDev::Role::HeuristicSet';
+requires 'heuristics', 'negative_heuristics';
+
+=method C<modules>
+
+Returns the list of fully qualified module names that comprise this heuristic.
+
+expands results from C<< ->heuristics >> and C<< ->negative_heuristics >>,
+with negative ones preceding positive.
+
+=cut
+
+sub modules {
+  my ($self) = @_;
+  my @out;
+  for my $heur ( $self->negative_heuristics ) {
+      push @out, $self->_expand_negative_heuristic($heur);
+  }
+  for my $heur ( $self->heuristics ) {
+    push @out, $self->_expand_heuristic($heur);
+  }
+  return @out;
+}
+
+1;
