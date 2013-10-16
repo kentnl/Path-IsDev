0b0b64a99d44ef8c6746c15022b905ed26bdac47
Kent Fredric <kentfredric@gmail.com>
2013-10-16 21:17:56 +1300

    Refactor: Reimplement Heuristic::Makefile in terms of new roles.
    Now does ::Heuristic and ::Matcher::Child::Exists::Any::File

--- lib/Path/IsDev/Heuristic/Makefile.pm
+++ lib/Path/IsDev/Heuristic/Makefile.pm
@@ -8,7 +8,10 @@ package Path::IsDev::Heuristic::Makefile;
 {
     "namespace":"Path::IsDev::Heuristic::Makefile",
     "interface":"single_class",
-    "does":"Path::IsDev::Role::Heuristic::AnyFile"
+    "does":[
+        "Path::IsDev::Role::Heuristic",
+        "Path::IsDev::Role::Matcher::Child::Exists::Any::File"
+    ]
 }
 
 =end MetaPOD::JSON
@@ -16,9 +19,8 @@ package Path::IsDev::Heuristic::Makefile;
 =cut
 
 # ABSTRACT: Determine if a path contains a C<Makefile>
-
 use Role::Tiny::With;
-with 'Path::IsDev::Role::Heuristic::AnyFile';
+with 'Path::IsDev::Role::Heuristic', 'Path::IsDev::Role::Matcher::Child::Exists::Any::File';
 
 =method C<files>
 
@@ -30,7 +32,24 @@ Files relevant to this heuristic:
 
 =cut
 
-sub files { return qw( GNUmakefile makefile Makefile ) }
+sub files {
+  return qw( GNUmakefile makefile Makefile );
+}
+
+=method C<matches>
+
+Matches if any of the files in C<files> exist as children of the C<path>
+
+=cut
+
+sub matches {
+  my ( $self, $result_object ) = @_;
+  if ( $self->child_exists_any_file( $result_object, $self->files ) ) {
+    $result_object->result(1);
+    return 1;
+  }
+  return;
+}
 
 1;
 
