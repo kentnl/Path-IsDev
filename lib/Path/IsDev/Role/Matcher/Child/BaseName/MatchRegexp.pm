2c5dc0d016cfd71598e150fc43e14b864d06b22e
Kent Fredric <kentfredric@gmail.com>
2013-10-16 20:55:53 +1300

    New Role: ::Matcher::Child::BaseName::MatchRegexp

---
+++ lib/Path/IsDev/Role/Matcher/Child/BaseName/MatchRegexp.pm
@@ -0,0 +1,69 @@
+use strict;
+use warnings;
+
+package Path::IsDev::Role::Matcher::Child::BaseName::MatchRegexp;
+
+# ABSTRACT: Match when a path has a child file matching an expression
+
+=begin MetaPOD::JSON v1.1.0
+
+{
+    "namespace":"Path::IsDev::Role::Matcher::Child::BaseName::MatchRegexp",
+    "interface":"role"
+}
+
+=end MetaPOD::JSON
+
+=cut
+
+use Role::Tiny;
+
+=p_method C<_this_child_matchregexp>
+
+    if ( $class->_this_child_matchregexp( $result_object, $child_path, $regexp ) ) {
+        ...
+    }
+
+=cut
+
+sub _this_child_matchregexp {
+  my ( $self, $result_object, $child, $regexp ) = @_;
+  my $ctx = {
+    'child'          => "$child",
+    'child_basename' => $child->basename,
+    expression       => $regexp,
+    tests            => []
+  };
+  my $tests = $ctx->{tests};
+
+  if ( $child->basename =~ $regexp ) {
+    push @{$tests}, { 'child_basename_matches_expression?' => 1 };
+    $result_object->add_reason( $self, 1, $child->basename . " matches $regexp", $ctx );
+    return 1;
+  }
+  push @{$tests}, { 'child_basename_matches_expression?' => 0 };
+  $result_object->add_reason( $self, 0, $child->basename . " does not match $regexp", $ctx );
+  return;
+}
+
+=method C<child_basename_matchregexp>
+
+    $class->child_basename_matchregexp( $result_object, $regexp );
+
+Given a regexp C<$regexp>, match if any of C<< $result_object->path->children >> match the given regexp.
+
+    if ( $self->child_basename_matchregexp( $result_object, qr/^Change(.*)$/i ) ) {
+        # result_object->path() contains at least one child that matches the regexp
+    }
+
+=cut
+
+sub child_basename_matchregexp {
+  my ( $self, $result_object, $regexp ) = @_;
+  for my $child ( $result_object->path->children ) {
+    return 1 if $self->_this_child_matchregexp( $result_object, $child, $regexp );
+  }
+  return;
+}
+
+1;
