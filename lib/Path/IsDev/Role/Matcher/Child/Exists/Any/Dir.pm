3cb6a5d72483797da99a1153d0d3ba60956cb556
Kent Fredric <kentfredric@gmail.com>
2013-10-16 21:04:59 +1300

    Refactor ::Role::Heuristic::AnyDir as ::Matcher::Child::BaseName::MatchRegexp::Any::Dir.
    Consumers of this role should now also directly `do` the relevant
    ::Role::(Negative|)Heursitic as well, and manually glue the two together

---
+++ lib/Path/IsDev/Role/Matcher/Child/Exists/Any/Dir.pm
@@ -0,0 +1,65 @@
+use strict;
+use warnings;
+
+package Path::IsDev::Role::Matcher::Child::Exists::Any::Dir;
+
+# ABSTRACT: Match if a path contains one of any of a list of directories
+
+=begin MetaPOD::JSON v1.1.0
+
+{
+    "namespace":"Path::IsDev::Role::Matcher::Child::Exists::Any::Dir",
+    "interface":"role",
+    "does":"Path::IsDev::Role::Matcher::Child::Exists::Any"
+}
+
+=end MetaPOD::JSON
+
+=cut
+
+use Role::Tiny;
+with 'Path::IsDev::Role::Matcher::Child::Exists::Any';
+
+=method C<child_exists_dir>
+
+    $class->child_exists_dir( $result_object, $childname );
+
+Return match if C<$childname> exists as a directory child of C<< $result_object->path >>,
+
+=cut
+
+sub child_exists_dir {
+  my ( $self, $result_object, $child ) = @_;
+
+  my $child_path = $result_object->path->child($child);
+  my $ctx        = { 'child_name' => $child, child_path => "$child_path", tests => [] };
+  my $tests      = $ctx->{tests};
+
+  if ( -d $child_path ) {
+    push @{$tests}, { 'child_path_isdir?' => 1 };
+    $result_object->add_reason( $self, 1, "$child_path is a dir", $ctx );
+    return 1;
+  }
+  push @{$tests}, { 'child_path_isdir?' => 0 };
+  $result_object->add_reason( $self, 0, "$child_path is not a dir", $ctx );
+
+  return;
+}
+
+=method C<child_exists_any_dir>
+
+    $class->child_exists_any_dir( $result_object, @childnames );
+
+Return match if any of C<@childnames> exist under C<< $result_object->path >> and are directories.
+
+=cut
+
+sub child_exists_any_dir {
+  my ( $self, $result_object, @children ) = @_;
+  for my $child (@children) {
+    return 1 if $self->child_exists( $result_object, $child ) and $self->child_exists_dir( $result_object, $child );
+  }
+  return;
+}
+
+1;
