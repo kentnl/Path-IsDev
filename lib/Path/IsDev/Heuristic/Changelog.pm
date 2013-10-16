1105b1a3a2e18d3dd1c61e90f29b7377b9d7e35e
Kent Fredric <kentfredric@gmail.com>
2013-10-16 21:21:22 +1300

    Refactor: Reimplement Heuristic::Changelog in terms of new roles.
    Now does ::Heuristic and ::Matcher::Child::BaseName::MatchRegexp::File

--- lib/Path/IsDev/Heuristic/Changelog.pm
+++ lib/Path/IsDev/Heuristic/Changelog.pm
@@ -25,7 +25,7 @@ etc.
 {
     "namespace":"Path::IsDev::Heuristic::Changelog",
     "interface":"single_class",
-    "does":"Path::IsDev::Role::Heuristic::RegexpFile"
+    "does":[ "Path::IsDev::Role::Heuristic", "Path::IsDev::Role::Matcher::Child::BaseName::MatchRegexp::File" ]
 }
 
 =end MetaPOD::JSON
@@ -33,7 +33,7 @@ etc.
 =cut
 
 use Role::Tiny::With;
-with 'Path::IsDev::Role::Heuristic::RegexpFile';
+with 'Path::IsDev::Role::Heuristic', 'Path::IsDev::Role::Matcher::Child::BaseName::MatchRegexp::File';
 
 =method C<basename_regexp>
 
@@ -48,6 +48,21 @@ Indicators for this heuristic is the existence of a file such as:
 
 sub basename_regexp {
   return qr/\AChange(s|log)(|[.][^.\s]+)\z/isxm;
+}
+
+=method C<matches>
+
+Returns a match if any child of C<path> exists matching the regexp C<basename_regexp>
+
+=cut
+
+sub matches {
+  my ( $self, $result_object ) = @_;
+  if ( $self->child_basename_matchregexp_file( $result_object, $self->basename_regexp ) ) {
+    $result_object->result(1);
+    return 1;
+  }
+  return;
 }
 
 1;
